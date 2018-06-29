7c48f3714a97bad349ddd6172c95736be471fde9
Putnam14 <putnambr76@gmail.com>
2018-06-28 22:23:46 -0600

    Updated main logic, need to further refactor VillageState.random out

--- EloquentJS/Chapter 7 - Project: A Robot/main.js
+++ EloquentJS/Chapter 7 - Project: A Robot/main.js
@@ -1,4 +1,22 @@
-const roads = require('./roads');
-const buildGraph = require('./buildGraph');
+const VillageState = require('./villageState');
+const randomPick = require('./randomPick');
+const roadGraph = require('./roadGraph');
+const runRobot = require('./runRobot');
+// const randomBot = require('./robots/randomRobot');
+const routeBot = require('./robots/routeRobot');
 
-const roadGraph = buildGraph(roads);
+VillageState.random = function randomVillageState(parcelCount = 5) {
+  const parcels = [];
+  for (let i = 0; i < parcelCount; i += 1) {
+    const address = randomPick(Object.keys(roadGraph));
+    let place;
+    do {
+      place = randomPick(Object.keys(roadGraph));
+    } while (place === address);
+    parcels.push({ place, address });
+  }
+  return new VillageState('Post Office', parcels);
+};
+
+// runRobot(VillageState.random(), randomBot);
+runRobot(VillageState.random(), routeBot, []);
