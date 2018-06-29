b0ce148c4229a81e0a6aafc53155307b4c4a1356
Putnam14 <putnambr76@gmail.com>
2018-06-28 22:24:46 -0600

    Run a mailbot

---
+++ EloquentJS/Chapter 7 - Project: A Robot/runRobot.js
@@ -0,0 +1,13 @@
+module.exports = function runRobot(state, robot, memory) {
+  let curr = state;
+  let mem = memory;
+  let turn = 0;
+  do {
+    const action = robot(curr, mem);
+    curr = curr.move(action.direction);
+    mem = action.memory;
+    console.log(`Moved to ${action.direction}`);
+    turn += 1;
+  } while (curr.parcels.length !== 0);
+  console.log(`Done in ${turn} turns!`);
+};
