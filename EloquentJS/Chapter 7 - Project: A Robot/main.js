886d58f9235f60bad29fd0e96d9b16a8d25066be
Putnam14 <putnambr76@gmail.com>
2018-06-30 00:00:53 -0600

    Complete compare robots exercise

--- EloquentJS/Chapter 7 - Project: A Robot/main.js
+++ EloquentJS/Chapter 7 - Project: A Robot/main.js
@@ -4,6 +4,8 @@ const roadGraph = require('./roadGraph');
 const runRobot = require('./runRobot');
 // const randomBot = require('./robots/randomRobot');
 const routeBot = require('./robots/routeRobot');
+const finderBot = require('./robots/finderBot');
+// const findRoute = require('./pathFind');
 
 VillageState.random = function randomVillageState(parcelCount = 5) {
   const parcels = [];
@@ -19,4 +21,19 @@ VillageState.random = function randomVillageState(parcelCount = 5) {
 };
 
 // runRobot(VillageState.random(), randomBot);
-runRobot(VillageState.random(), routeBot, []);
+// runRobot(VillageState.random(), routeBot, []);
+runRobot(VillageState.random(), finderBot, []);
+
+function compareBots(robot1, memory1, robot2, memory2) {
+  let r1 = 0;
+  let r2 = 0;
+  for (let i = 0; i < 100; i += 1) {
+    const randState = VillageState.random();
+    r1 += runRobot(randState, robot1, memory1);
+    r2 += runRobot(randState, robot2, memory2);
+  }
+  console.log(`Robot number one took ${r1 / 100} turns on average.`);
+  console.log(`Robot two took ${r2 / 100} turns on average`);
+}
+
+compareBots(routeBot, [], finderBot, []);
