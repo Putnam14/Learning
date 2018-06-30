439ba9761d541f0f1444d73e295b6e4144311488
Putnam14 <putnambr76@gmail.com>
2018-06-30 00:01:26 -0600

    Make path finder work

--- EloquentJS/Chapter 7 - Project: A Robot/pathFind.js
+++ EloquentJS/Chapter 7 - Project: A Robot/pathFind.js
@@ -2,12 +2,11 @@ module.exports = function findRoute(graph, from, to) {
   const work = [{ at: from, route: [] }];
   for (let i = 0; i < work.length; i += 1) {
     const { at, route } = work[i];
-    graph[at].forEach((p) => {
-      if (p === to) return route.concat(p);
-      if (!work.some(w => w.at === p)) {
-        work.push({ at: p, route: route.concat(p) });
+    for (const place of graph[at]) { // Need to fix this to be airbnb compliant
+      if (place === to) return route.concat(place);
+      if (!work.some(w => w.at === place)) {
+        work.push({ at: place, route: route.concat(place) });
       }
-      return false;
-    });
+    }
   }
 };
