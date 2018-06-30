20dcca32d8a3d15f9dfb24ff6e4b23e646ed1e98
Putnam14 <putnambr76@gmail.com>
2018-06-29 20:38:28 -0600

    Refactor

--- EloquentJS/Chapter 7 - Project: A Robot/pathFind.js
+++ EloquentJS/Chapter 7 - Project: A Robot/pathFind.js
@@ -2,11 +2,13 @@ module.exports = function findRoute(graph, from, to) {
   const work = [{ at: from, route: [] }];
   for (let i = 0; i < work.length; i += 1) {
     const { at, route } = work[i];
-    for (let place of graph[at]) {
-      if (place == to) return route.concat(place);
-      if (!work.some(w => w.at == place)) {
-        work.push({at: place, route: route.concat(place)});
+    graph[at].forEach((p) => {
+      if (!(p === to)) {
+        if (!work.some(w => w.at === p)) {
+          work.push({ at: p, route: route.concat(p) });
+        }
       }
-    }
+      return route.concat(p);
+    });
   }
 };
