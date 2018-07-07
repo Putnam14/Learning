ef71fb640baef46e502b08673abb69f94114b5b6
Putnam14 <putnambr76@gmail.com>
2018-07-06 22:20:36 -0600

    Move item cases to above their code

--- EloquentJS/Chapter 9 - Regular Expressions/regexGolf.js
+++ EloquentJS/Chapter 9 - Regular Expressions/regexGolf.js
@@ -4,23 +4,7 @@
  * of the substrings described. Do not worry about word boundaries unless explicitly mentioned.
  *
  * When your expression works, see whether you can make it any smaller.
- *
- * 1. car and cat
- *
- * 2. pop and prop
- *
- * 3. ferret, ferry, and ferrari
- *
- * 4. Any word ending in ious
- *
- * 5. A whitespace character followed by a period, comma, colon, or semicolon
- *
- * 6. A word longer than six letters
- *
- * 7. A word without the letter e (or E)
  */
-
-// Fill in the regular expressions
 
 function verify(regexp, yes, no) {
   if (regexp.source === '...') return;
@@ -36,30 +20,37 @@ function verify(regexp, yes, no) {
   });
 }
 
+// 1. car and cat
 verify(/ca(r|t)/,
   ['my car', 'bad cats'],
   ['camper', 'high art']);
 
+// 2. pop and prop
 verify(/pr?op/,
   ['pop culture', 'mad props'],
   ['plop', 'prrrop']);
 
+// 3. ferret, ferry, and ferrari
 verify(/ferr(et|y|ari)/,
   ['ferret', 'ferry', 'ferrari'],
   ['ferrum', 'transfer A']);
 
+// 4. Any word ending in ious
 verify(/ious\b/,
   ['how delicious', 'spacious room'],
   ['ruinous', 'consciousness']);
 
+// 5. A whitespace character followed by a period, comma, colon, or semicolon
 verify(/\s[.,:;]/,
   ['bad punctuation .'],
   ['escape the period']);
 
+// 6. A word longer than six letters
 verify(/\w{7,}/,
   ['hottentottententen'],
   ['no', 'hotten totten tenten']);
 
+// 7. A word without the letter e (or E)
 verify(/^(.+ [^e]+|[^e]+ .+)$/i,
   ['red platypus', 'wobbling nest'],
   ['earth bed', 'learning ape', 'BEET']);
